32dbc92d96d285e57a02802d386053e60f9f6f54
Jamie Ralph <jamesryanralph@outlook.com>
2020-03-20 19:20:12 +0000

    initial commit

---
+++ prepare_data.R
@@ -0,0 +1,60 @@
+### Ideas script ### 
+library(tidyverse)
+
+# Choose the columns we want
+
+columns <-  c("Respondent",
+                  "MainBranch",
+                  "Hobbyist",
+                  "OpenSourcer",
+                  "Employment",
+                  "Country",
+                  "UndergradMajor",
+                  "DevType",
+                  "YearsCode",
+                  "JobSat",
+                  "ConvertedComp",
+                  "WorkRemote",
+                  "LanguageWorkedWith",
+                  "LanguageDesireNextYear",
+                  "Age",
+                  "Gender",
+                  "Trans",
+                  "Sexuality")
+
+# Read the data in
+
+df <- read_csv("survey_results_public.csv") %>%
+    select(columns) %>%
+    filter(Employment == "Employed full-time",
+        ConvertedComp > 3e4,
+        ConvertedComp < 2e6)
+
+
+# Filter to just UK and Ireland data people
+
+data_jobs_UKIR <- df %>% 
+    mutate(DevType = str_split(DevType, pattern = ";")) %>%
+    unnest(DevType) %>%
+    mutate(DevType = case_when(
+        str_detect(str_to_lower(DevType), "data scientist") ~ "Data scientist",
+        str_detect(str_to_lower(DevType), "data or business") ~ "Data analyst",
+        TRUE ~ "Other"
+    )) %>%
+    filter(DevType != "Other")
+
+data_jobs_UKIR %>%
+    ggplot(aes(ConvertedComp, colour = DevType, fill = DevType)) +
+    geom_density(alpha = 0.4, size = 1) +
+    scale_x_log10(labels = scales::dollar_format()) +
+    theme_minimal() +
+    labs(
+        x = "Salary (USD)",
+        y = "Density",
+        fill = "Developer type",
+        colour = "Developer type"
+    )
+    
+
+
+        
